d6e2c230a3934654694dc319959f14aa6d12e98f
Aghgh <The_A@gmx.net>
2025-05-20 22:22:24 +0200

    formatting and comments

--- getLatestBigTrades.js
+++ getLatestBigTrades.js
@@ -1,23 +1,23 @@
 async function main() {
-  let betCount = 400;
+  const AMOUNT_OF_BETS_TO_LOAD = 400;
 
-  // bots, sports-bettors, etc.
-  let ignoreUsers = ["FDWTsTHFytZz96xmcKzf7S5asYL2", "qnIAzz9RamaodeiJSiGZO6xRGC63" // Yuna , Agh
+  /** users where you are less likely to benefit from correcting their trades (e.g. good users, news-traders, bots), or I am not as interested (sports-bettors) */
+  const ignoreUsers = ["FDWTsTHFytZz96xmcKzf7S5asYL2", "qnIAzz9RamaodeiJSiGZO6xRGC63" // Yuna , Agh
     ,"Iiok8KHMCRfUiwtMq1tl5PeDbA73", "EJQOCF3MfLTFYbhiKncrNefQDBz1"]; // Lion, chrisjbillington
-  // users where you are less likely to benefit from correcting their trades (e.g. good users, news-traders, bots)
-  let lessInterestingUsers = ["ilJdhpLzZZSUgzueJOs2cbRnJn82", // Botlab
+  // see above
+  const lessInterestingUsers = ["ilJdhpLzZZSUgzueJOs2cbRnJn82", // Botlab
     "BhNkw088bMNwIFF2Aq5Gg9NTPzz1", "BgCeVUcOzkexeJpSPRNomWQaQaD3", "xB6IgHFizCHEJwqZ3un3", // acc, SemioticRivalry, mattyB
-    "JlVpsgzLsbOUT4pajswVMr0ZzmM2", "sTUV8ejuM2byukNZp7qKP2OKXMx2"]; // joshua, NFL Unofficial
-  let uninterestingMarkets = [
-    "y8thPELtPE", // why did democrate lose the election
+    "JlVpsgzLsbOUT4pajswVMr0ZzmM2"]; // joshua
+  const uninterestingMarkets = [
     "0y4cve16lz", // dwarkesh podcast
     "WCsjjEUk1vxRy1wHNi63" // Eliezer UFO bet
     ]; 
-  let uninterestingMarketGroups = ["sports-default","sports-betting","nfl","auto-racing","one-piece-stocks","stocks"]
+  /** personal disinterest */
+  const uninterestingMarketGroups = ["sports-default","sports-betting","nfl","auto-racing","one-piece-stocks","stocks"]
 
-  let interestingBetTreshold = 1.7;
+  const INTERESTING_BET_TRESHOLD = 1.7;
 
-  // how significant or exploitable the bet is
+  /** how significant or exploitable the bet is */
   function rateBet(bet) {
     let rating = 0;
     rating += Math.log10(bet._movement * 2 + 0.1);
@@ -31,7 +31,7 @@ async function main() {
     if (bet._isSold && bet._absAmount > 100) { rating += 0.7; } // sells are more likely to be non-epistemic
     if (bet.isApi) { rating -= 0.1; } // bots are harder to exploit
     rating -= bet._ageInMinutes / 60;
-    if (bet._ageInMinutes <= 2) { rating += 0.2 }
+    if (bet._ageInMinutes <= 2) { rating += 0.2 } // the more time passed after a bad bet, the more likely it is that somebody else already corrected it.
     if (bet.answerId) { rating -= 0.3; } // personal dislike for multi-choice markets.
     let cachedMarket = globalThis.marketCache[bet.contractId];
     if (cachedMarket) {
@@ -51,7 +51,8 @@ async function main() {
     console.log("rateBetBasedOnMarket ", cachedMarket.question.toLowerCase() )
     if (cachedMarket.isResolved){ bet._significance-=3;}
     if (cachedMarket.question.toLowerCase().includes(" stock")) {bet._significance-=0.1;} // Stocks are boring
-    if (cachedMarket.question.includes(" 2030")) {bet._significance-=0.2;}
+    if (cachedMarket.question.includes(" 2030")) {bet._significance-=0.2;} // far away markets have worse ROI
+    // TODO: test removing this
     if (cachedMarket.uniqueBettorCount<= 5) {bet._significance-=0.5;}
     if (cachedMarket.uniqueBettorCount<= 10) {bet._significance-=0.2;}
      // Left-Wing or Right-Wing? Which person/character/concept will Manifold think are "Right-Wing" this week?"
@@ -60,33 +61,34 @@ async function main() {
   }
 
   function output(...text) {
-    outputNode('pre', false, ...text)
+    logToHtml('pre', false, ...text)
   }
   function outputBold(...text) {
-    outputNode('pre', true, ...text)
+    logToHtml('pre', true, ...text)
   }
   function outputLink(link, text) {
-    outputNode('a', true, link, text)
+    logToHtml('a', true, link, text)
   }
 
-  function outputNode(node, bold, ...t) {
-    const text = t.join('  ');
-    console.log(text)
+  function logToHtml(tagName, isBold, ...textBits) {
+    const joinedText = textBits.join('  ');
+    console.log(joinedText)
     if (!globalThis.document?.querySelector('#output')) return;
-    const newNode = document.createElement(node);
-    if (node === 'a') {
-      newNode.href = t[0];
-      newNode.innerText = t[1];
+    const newHTMLElement = document.createElement(tagName);
+    if (tagName === 'a') {
+      newHTMLElement.href = textBits[0];
+      newHTMLElement.innerText = textBits[1];
     } else {
-      newNode.innerText = text;
+      newHTMLElement.innerText = joinedText;
     }
 
-    if (bold) {
-      newNode.style.fontWeight = "bold"
+    if (isBold) {
+      newHTMLElement.style.fontWeight = "bold"
     }
-    document.querySelector('#output').appendChild(newNode)
+    document.querySelector('#output').appendChild(newHTMLElement)
   }
 
+  /** for debugging and checking how meaningful certain criteria are */
   function collectStats(bets) {
     let stats = {
       "amount >=500:": bets.filter(x => (x._absAmount) >= 500).length,
@@ -118,15 +120,15 @@ async function main() {
 
   function roundTo2(num) { return Math.round(num * 100) / 100 }
 
-  // merge consecutive bets regardless of user
-  // not sure if this works correctly
+  /**  merge consecutive bets regardless of user
+   not sure if this works correctly */
   function mergeConsecutiveBets(bets) {
     let aggregatedBets = [];
     for (let i = 0; i < bets.length; i++) {
       let bet = bets[i];
       let mergableBet = aggregatedBets.find((b) => b.contractId == bet.contractId
         && b.answerId == bet.answerId
-        && b.outcome == bet.outcome // math for calculating value of merged yes and no bets is too difficult.
+        && b.outcome == bet.outcome // math for calculating value of merged yes and no bets is too difficult. So I dont bother
         && Math.abs(b._probAfter - bet._probBefore) <= 0.02
       );
       if (mergableBet) {
@@ -147,20 +149,20 @@ async function main() {
   }
 
   globalThis.marketCache = globalThis.marketCache || {}
-  async function loadMarket(contractId) {
+  async function loadAndCacheMarket(contractId) {
     let market;
     if (marketCache[contractId]) {
-      market = marketCache[contractId]; // save on API-Calls
+      market = marketCache[contractId]; // save on API-Calls and time
     } else {
-      let response2 = await fetch("https://api.manifold.markets/v0/market/" + contractId, { method: "GET" })
-      market = await response2.json();
+      let response = await fetch("https://api.manifold.markets/v0/market/" + contractId, { method: "GET" })
+      market = await response.json();
       marketCache[contractId] = market;
     }
     return market;
   }
 
   async function outputBetAndMarketInfo(bet) {
-    let market = await loadMarket(bet.contractId);
+    let market = await loadAndCacheMarket(bet.contractId);
 
     let realcurrentProb = market.probability && roundTo2(market.probability * 100);
     if (bet.answerId) {
@@ -170,14 +172,15 @@ async function main() {
     outputLink(market.url, market.question + " (" + answer + ") ")
 
     if (market.isResolved) {
+      // You could also opt to not log anything. But sometimes it's kinda, sort of interesting. And it doesn't happen often enough to annoy me.
       outputBold("TOO LATE! market resolved!")
       output(" ");
       return;
     }
     if (realcurrentProb && Math.abs(realcurrentProb - bet._probAfter) > 0.9
-      && /*bugfix because sometimes you get the old cached probability*/ realcurrentProb !== bet.probBefore) {
+      && /* bugfix because sometimes you get the old cached probability */ realcurrentProb !== bet.probBefore) {
       outputBold("market moved more afterwards, current percentage:",
-        realcurrentProb)//, " (_movement:",roundTo2(Math.abs(realcurrentProb-bet.probBefore)),")")
+        realcurrentProb)
     }
     if (market.uniqueBettorCount <= 10) {
       outputBold("low Trader count market ("+ market.uniqueBettorCount+")!")
@@ -187,19 +190,21 @@ async function main() {
       " significance:", roundTo2(bet._significance),)
     output("prob:   ", (bet._probBefore), " -> ", (bet._probAfter),
       (bet._ageInMinutes <= 4) ? "" : (" [placed " + bet._ageInMinutes + " minutes ago]"))
-    console.log("user: ", bet.userName || bet.userUsername || bet.userId)
+    console.log("userId: ", bet.userId)
 
-    console.log("marketId",bet.contractId);
+    console.log("marketId: ",bet.contractId);
     output(" ");
   }
 
+  // -----------------------------------------
   // START
+  // -----------------------------------------
   try {
     if (globalThis.document?.querySelector('#output')) {
       document.querySelector('#output').innerHTML = "";
     }
     let bets = [];
-    const response = await fetch("https://api.manifold.markets/v0/bets?order=desc&limit=" + betCount, { method: "GET" })
+    const response = await fetch("https://api.manifold.markets/v0/bets?order=desc&limit=" + AMOUNT_OF_BETS_TO_LOAD, { method: "GET" })
     bets = await response.json();
     if (!Array.isArray(bets)) {
       throw "Unknown API-Error";
@@ -221,7 +226,7 @@ async function main() {
     //collectStats(bets);
 
     bets = bets.filter(bet => bet._absAmount !== 0);
-    console.log("filtered out " + (betCount - bets.length) + " nothing bets")
+    console.log("filtered out " + (AMOUNT_OF_BETS_TO_LOAD - bets.length) + " nothing bets")
     let newCount = bets.length
     //let bets2 = mergeConsecutiveBets(bets);
     //output("bets merged "+(newCount - bets2.length));
@@ -244,26 +249,26 @@ async function main() {
 
     // preload Markets for the most noteworthy bets (in parallel for speed)
     bets = bets.sort((a, b) => b._significance - a._significance);
-    const marketsToLoad = Array.from(new Set(bets.slice(0, 8+2).filter(bt => bt._significance > interestingBetTreshold).map(b =>b.contractId)))
-    await Promise.all(marketsToLoad.map(id=>loadMarket(id)));
+    const marketsToLoad = Array.from(new Set(bets.slice(0, 8+2).filter(bt => bt._significance > INTERESTING_BET_TRESHOLD).map(b =>b.contractId)))
+    await Promise.all(marketsToLoad.map(id=>loadAndCacheMarket(id)));
 
     bets.forEach(bet=>rateBetBasedOnMarket(bet));
     bets = bets.sort((a, b) => b._significance - a._significance);
 
     let mostSignificantBet = bets[0]; // biggest bet by _significance
 
-    bets = bets.filter(bt => bt._significance > interestingBetTreshold)
+    bets = bets.filter(bt => bt._significance > INTERESTING_BET_TRESHOLD)
 
     //collectStats(bets);
 
     let topX = Math.min(bets.length ,8);
 
-    output("noteworthy bets count:", bets.length, "(out of " + betCount + " total)")
+    output("This script loaded the last " + AMOUNT_OF_BETS_TO_LOAD + " bets. Out of those " + bets.length  + " were deemed significant.")
     if (bets.length == 0) {
       outputBold("only boring bets! most significant bet:")
       bets = [mostSignificantBet]
     } else {
-      output("TOP " + topX + " bets:")
+      output("TOP " + topX + " most significant bets:")
     }
 
     bets = bets.slice(0, topX);
